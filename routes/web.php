e3d206cc7ee3e88d1a35d9e44d22f50ec476c221
Bishalkl <Bishal123koirala@gmail.com>
2025-01-22 15:38:50 +0545

    done

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/login', [AuthController::class, 'login']);
-Route::get('/', [AuthController::class, 'home']);
-
+route::get('/', function (){
+    return "Hello world";
+});
