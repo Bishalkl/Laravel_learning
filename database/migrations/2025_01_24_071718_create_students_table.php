ac255acf2bab80c4204fa47146356c2497b818a8
Bishalkl <Bishal123koirala@gmail.com>
2025-01-24 13:11:23 +0545

    done

--- database/migrations/2025_01_24_071718_create_students_table.php
+++ database/migrations/2025_01_24_071718_create_students_table.php
@@ -11,10 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('city', function (Blueprint $table) {
-            $table->integer('id');
-            $table->string('name')->unique();
-            $table->primary('id');
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 30);
+            $table->string('email', 40)->nullable()->unique();
+            $table->timestamps();
         });
     }
 
@@ -23,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('city');
+        Schema::dropIfExists('students');
     }
 };
